bd76ce7d1bde465dab29b66c2a5181da623d44e0
basarekinci <basarekinci02@outlook.com>
2025-07-27 17:10:25 +0300

    Level Datas Instances Updated

--- Assets/GameFolders/Scripts/ScriptableObjects/LevelDataSO.cs
+++ Assets/GameFolders/Scripts/ScriptableObjects/LevelDataSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using GameFolders.Scripts.Data;
 using GameFolders.Scripts.Enums;
@@ -9,14 +10,14 @@ namespace GameFolders.Scripts.ScriptableObjects
     [CreateAssetMenu(fileName = "LevelDataSO", menuName = "ScriptableObjects/LevelDataSO")]
     public class LevelDataSO : ScriptableObject
     {
-        [InfoBox("For luggage target count must be power of 3 (3, 9, 27, etc.)")]
+        [InfoBox("Luggage target count must be power of 3 (3, 9, 27, etc.)")]
         
         [Header("Level Configuration")]
         [Tooltip("Seconds")]
         [SerializeField] private float levelTimeInSeconds;
         [SerializeField] private List<TargetLuggageInfo> targetLuggageInfo;
         [Tooltip("Different luggage types to spawn current level")]
-        [SerializeField] private List<LuggageType> luggageTypes;
+        [SerializeField] private List<LuggageType> luggageTypesToSpawn;
         
         [Header("Collectible Configuration")]
         [SerializeField] private List<CollectablePieceInfo> collectablePieceInfo;
@@ -25,10 +26,11 @@ namespace GameFolders.Scripts.ScriptableObjects
         [Header("Junk Configuration")]
         [SerializeField] private List<JunkPieceInfo> junkPieceInfo;
         
-        public List<LuggageType> LuggageTypes => luggageTypes;
+        public List<LuggageType> LuggageTypesToSpawn => luggageTypesToSpawn;
         public List<CollectablePieceInfo> CollectablePieceInfo => collectablePieceInfo;
         public List<TargetLuggageInfo> TargetLuggageInfo => targetLuggageInfo;
         public bool HasCollectiblePiece => hasCollectiblePiece;
         public float TimeInSeconds => levelTimeInSeconds;
+        
     }
 }
