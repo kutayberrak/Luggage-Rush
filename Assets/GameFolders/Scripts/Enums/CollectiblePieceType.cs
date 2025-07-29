ac00ba8dc73e3f51e548b7470ac0d0ceb1f3a9ab
Eren ŞİŞMAN <93434398+ERENSISMAN61@users.noreply.github.com>
2025-07-30 00:43:54 +0300

    collection and garbage models added

--- Assets/GameFolders/Scripts/Enums/CollectiblePieceType.cs
+++ Assets/GameFolders/Scripts/Enums/CollectiblePieceType.cs
@@ -6,6 +6,17 @@
         CollectiblePieceType1,
         CollectiblePieceType2,
         CollectiblePieceType3,
-        CollectiblePieceType
+        CollectiblePieceType4,
+        CollectiblePieceType5,
+        CollectiblePieceType6,
+        CollectiblePieceType7,
+        CollectiblePieceType8,
+        CollectiblePieceType9,
+        CollectiblePieceType10,
+        CollectiblePieceType11,
+        CollectiblePieceType12,
+        CollectiblePieceType13,
+        CollectiblePieceType14,
+        CollectiblePieceType15,
     }
 }
