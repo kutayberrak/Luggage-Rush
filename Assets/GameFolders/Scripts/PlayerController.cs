065753aa5c7dacad0dd9a4338cc54f7d77fa7859
Kutay Berrak <127015869+kutayberrak@users.noreply.github.com>
2025-07-30 22:03:09 +0300

    Add Outline on Hold

--- Assets/GameFolders/Scripts/PlayerController.cs
+++ Assets/GameFolders/Scripts/PlayerController.cs
@@ -2,27 +2,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private LayerMask clickableLayer; // sadece bu layer'ý tarar
+    [SerializeField] private LayerMask clickableLayer;
+    [SerializeField] private float maxDistance = 100f;
+
+    private ClickableObject currentHover;
+    private MaterialPropertyBlock mpb;
+    public Camera mainCam;
+
+    private void Awake()
+    {
+        mpb = new MaterialPropertyBlock();
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Sol týklama
+        if (Input.GetMouseButton(0))
         {
-            HandleClick();
+            UpdateHover();
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (currentHover != null)
+            {
+                currentHover.OnClickedByPlayer();
+                ClearOutline(currentHover);
+                currentHover = null;
+            }
         }
     }
 
-    private void HandleClick()
+    private void UpdateHover()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f, clickableLayer))
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit, maxDistance, clickableLayer))
         {
-            ClickableObject clickable = hitInfo.collider.GetComponent<ClickableObject>();
-            if (clickable != null)
+            var now = hit.collider.GetComponent<ClickableObject>();
+            if (now != currentHover)
             {
-                clickable.OnClickedByPlayer();
+                if (currentHover != null)
+                    ClearOutline(currentHover);
+
+                currentHover = now;
+                if (currentHover != null)
+                    SetOutline(currentHover, 1f);
             }
         }
+        else
+        {
+            if (currentHover != null)
+            {
+                ClearOutline(currentHover);
+                currentHover = null;
+            }
+        }
+    }
+
+    private void SetOutline(ClickableObject obj, float value)
+    {
+        var rend = obj.GetComponent<Renderer>();
+        rend.GetPropertyBlock(mpb);
+        mpb.SetFloat("_OutlineMultiplier", value);
+        rend.SetPropertyBlock(mpb);
+    }
+
+    private void ClearOutline(ClickableObject obj)
+    {
+        SetOutline(obj, 0f);
     }
 }
