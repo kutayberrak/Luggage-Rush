efed93d59ed009dee557bf8d88d005490aee49a6
Eren ŞİŞMAN <93434398+ERENSISMAN61@users.noreply.github.com>
2025-07-31 22:01:10 +0300

    conveyor belt moving anim added

--- Assets/GameFolders/Scripts/ConveyorBeltController.cs
+++ Assets/GameFolders/Scripts/ConveyorBeltController.cs
@@ -8,11 +8,35 @@ namespace GameFolders.Scripts
         [SerializeField] public float conveyorSpeed = 1f;
         [SerializeField] private Rigidbody conveyorRigidbody;
 
+        [Header("Material Animation")]
+        [SerializeField] private Renderer conveyorRenderer;
+        [SerializeField] private float textureScrollSpeed = 1f;
+
+        private string texturePropertyName = "_BaseMap";
+        private Material conveyorMaterial;
+        private float textureOffset = 0f;
+
+        void Start()
+        {
+            conveyorMaterial = conveyorRenderer.material;
+        }
+
         void FixedUpdate()
         {
             Vector3 pos = conveyorRigidbody.position;
             conveyorRigidbody.position -= transform.right * Time.fixedDeltaTime * conveyorSpeed; // Conveyor hareketi
             conveyorRigidbody.MovePosition(pos);
+
+            AnimateConveyorTexture();
         }
+
+        private void AnimateConveyorTexture()
+        {
+            textureOffset += Time.fixedDeltaTime * textureScrollSpeed * conveyorSpeed;
+
+            Vector2 offset = new Vector2(0, textureOffset);
+            conveyorMaterial.SetTextureOffset(texturePropertyName, offset);
+        }
+
     }
 }
