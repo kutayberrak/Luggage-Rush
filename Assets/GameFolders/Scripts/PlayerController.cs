8295fed8a2079c08e39b8cd47e9f7762f141ff74
Kutay Berrak <127015869+kutayberrak@users.noreply.github.com>
2025-07-31 20:33:58 +0300

    Add Lift Effect on Hold for Objects

--- Assets/GameFolders/Scripts/PlayerController.cs
+++ Assets/GameFolders/Scripts/PlayerController.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private LayerMask clickableLayer;
-    [SerializeField] private float maxDistance = 100f;
+    [SerializeField] private Camera mainCam;
+    private float maxDistance = 100f;
+    [SerializeField] private float liftOffset = 0.5f;    // Height to lift on hold
+    private float holdThreshold = 0.1f;  // Time to distinguish hold vs click
 
-    private ClickableObject currentHover;
+    private ClickableObject currentClickable;
     private MaterialPropertyBlock mpb;
-    public Camera mainCam;
+    private Dictionary<ClickableObject, float> originalHeights = new Dictionary<ClickableObject, float>();
+    private float hoverStartTime;
+    private bool isHolding;
 
     private void Awake()
     {
@@ -16,46 +22,73 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            UpdateHover();
+            DoRaycast(true);
         }
-
+        else if (Input.GetMouseButton(0))
+        {
+            DoRaycast(false);
+        }
         else if (Input.GetMouseButtonUp(0))
         {
-            if (currentHover != null)
+            if (currentClickable != null)
             {
-                currentHover.OnClickedByPlayer();
-                ClearOutline(currentHover);
-                currentHover = null;
+                currentClickable.OnClickedByPlayer();
+                ClearOutline(currentClickable);
+                currentClickable = null;
+                isHolding = false;
             }
         }
     }
 
-    private void UpdateHover()
+    private void DoRaycast(bool isDown)
     {
         Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out var hit, maxDistance, clickableLayer))
         {
-            var now = hit.collider.GetComponent<ClickableObject>();
-            if (now != currentHover)
+            var obj = hit.collider.GetComponent<ClickableObject>();
+            if (obj != currentClickable)
             {
-                if (currentHover != null)
-                    ClearOutline(currentHover);
+                if (currentClickable != null)
+                    ClearOutline(currentClickable);
 
-                currentHover = now;
-                if (currentHover != null)
-                    SetOutline(currentHover, 1f);
+                currentClickable = obj;
+                hoverStartTime = Time.time;
+
+                SetOutline(currentClickable, 1f);
+                isHolding = false;
             }
-        }
-        else
-        {
-            if (currentHover != null)
+
+            if (!isHolding)
             {
-                ClearOutline(currentHover);
-                currentHover = null;
+                if (Time.time - hoverStartTime >= holdThreshold)
+                {
+                    StartHolding(currentClickable);
+                }
+            }
+            else
+            {
+                LiftObject(currentClickable);
             }
         }
+        else if (isDown || isHolding)
+        {
+            if (currentClickable != null)
+            {
+                ClearOutline(currentClickable);
+                currentClickable = null;
+                isHolding = false;
+            }
+        }
+    }
+
+    private void StartHolding(ClickableObject obj)
+    {
+        if (obj == null) return;
+        isHolding = true;
+        RecordOriginalHeight(obj);
+        LiftObject(obj);
     }
 
     private void SetOutline(ClickableObject obj, float value)
@@ -69,5 +102,30 @@ public class PlayerController : MonoBehaviour
     private void ClearOutline(ClickableObject obj)
     {
         SetOutline(obj, 0f);
+        ResetObjectPosition(obj);
+    }
+
+    private void RecordOriginalHeight(ClickableObject obj)
+    {
+        if (obj != null && !originalHeights.ContainsKey(obj))
+            originalHeights[obj] = obj.transform.position.y;
+    }
+
+    private void LiftObject(ClickableObject obj)
+    {
+        if (obj == null) return;
+        Vector3 pos = obj.transform.position;
+        float baseY = originalHeights[obj];
+        obj.transform.position = new Vector3(pos.x, baseY + liftOffset, pos.z);
+    }
+
+    private void ResetObjectPosition(ClickableObject obj)
+    {
+        if (obj != null && originalHeights.TryGetValue(obj, out float baseY))
+        {
+            Vector3 pos = obj.transform.position;
+            obj.transform.position = new Vector3(pos.x, baseY, pos.z);
+            originalHeights.Remove(obj);
+        }
     }
 }
