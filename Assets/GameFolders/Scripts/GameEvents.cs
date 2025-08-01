501a4c799ae011e23d9bd7cc4432c1bfec1420db
Eren ŞİŞMAN <93434398+ERENSISMAN61@users.noreply.github.com>
2025-08-02 01:31:14 +0300

    win/fail bug fixed

--- Assets/GameFolders/Scripts/GameEvents.cs
+++ Assets/GameFolders/Scripts/GameEvents.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace GameFolders.Scripts
 {
@@ -9,11 +10,36 @@ namespace GameFolders.Scripts
         public static event Action OnGameStart;
         public static event Action OnReturnToMainMenu;
         public static event Action OnLevelRestarted;
-        
+
+        // Cooldown variables for level end events
+        private static float lastLevelEndTime = -1f;
+        private static readonly float levelEndCooldown = 2f; // 2 seconds cooldown
+
         public static void TriggerLevelRestarted() => OnLevelRestarted?.Invoke();
-        public static void TriggerReturnToMainMenu()=>OnReturnToMainMenu?.Invoke();
+        public static void TriggerReturnToMainMenu() => OnReturnToMainMenu?.Invoke();
         public static void TriggerGameStart() => OnGameStart?.Invoke();
-        public static void TriggerLevelWin() => OnLevelWin?.Invoke();
-        public static void TriggerLevelFailed() => OnLevelFailed?.Invoke();
+
+        public static void TriggerLevelWin()
+        {
+            if (CanTriggerLevelEndEvent())
+            {
+                lastLevelEndTime = Time.time;
+                OnLevelWin?.Invoke();
+            }
+        }
+
+        public static void TriggerLevelFailed()
+        {
+            if (CanTriggerLevelEndEvent())
+            {
+                lastLevelEndTime = Time.time;
+                OnLevelFailed?.Invoke();
+            }
+        }
+
+        private static bool CanTriggerLevelEndEvent()
+        {
+            return lastLevelEndTime < 0f || (Time.time - lastLevelEndTime) >= levelEndCooldown;
+        }
     }
 }
