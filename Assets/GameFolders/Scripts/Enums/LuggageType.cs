6e31139dd3dd4642ffef1e62fb824237dd72fcb4
Kutay Berrak <127015869+kutayberrak@users.noreply.github.com>
2025-08-02 17:43:04 +0300

    France Prefabs Added
    France Level Datas Updated

--- Assets/GameFolders/Scripts/Enums/LuggageType.cs
+++ Assets/GameFolders/Scripts/Enums/LuggageType.cs
@@ -3,11 +3,15 @@
     public enum LuggageType
     {
         None,
-        LuggageType1,
-        LuggageType2,
-        LuggageType3,
-        LuggageType4,
-        LuggageType5,
-        LuggageType6,
+        Luggage_1_1_Blue,
+        Luggage_1_2_Green,
+        Luggage_1_3_Orange,
+        Luggage_1_4_Red,
+        Luggage_1_5_Yellow,
+        Luggage_2_1_Pink,
+        Luggage_2_2_Red,
+        Luggage_2_3_Purple_Thin,
+        Luggage_2_4_Purple,
+        Luggage_2_5_Brown,
     }
 }
