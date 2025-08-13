11ba6438226f9e23b39bfe1e0b53b7cd7dd2d815
Kutay Berrak <127015869+kutayberrak@users.noreply.github.com>
2025-08-13 20:49:58 +0300

    Bomb applies force to nearby objects.

--- Assets/GameFolders/Scripts/ItemScripts/Bomb.cs
+++ Assets/GameFolders/Scripts/ItemScripts/Bomb.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 public class Bomb : SpecialItem
 {
     [SerializeField] private float timeToRemove = 5f;
+    [SerializeField] private float explosionForce = 250f;
+    [SerializeField] private float explosionRadius = 10f;
+
     public override void OnClickedByPlayer()
     {
         Timer.Instance.RemoveTime(timeToRemove);
+
+        Explode();
+
         ObjectPoolManager.Instance.ReturnObjectToPool(gameObject);
     }
 
     protected override void StartCurveMovement()
     {
-
     }
+
     protected override void StartClickAnimation()
     {
+    }
 
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider nearby in colliders)
+        {
+            Rigidbody rb = nearby.attachedRigidbody;
+            if (rb != null && rb != this.GetComponent<Rigidbody>())
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+        }
     }
 }
