5443fa430e3c1518d78a24c5616ca3f87e2bff8f
Eren ŞİŞMAN <93434398+ERENSISMAN61@users.noreply.github.com>
2025-08-13 23:46:13 +0300

    tutorial bg alpha updated

--- Assets/GameFolders/Scripts/Tutorial/TutorialManager.cs
+++ Assets/GameFolders/Scripts/Tutorial/TutorialManager.cs
@@ -3,20 +3,19 @@ using TMPro;
 using GameFolders.Scripts;
 using GameFolders.Scripts.Managers;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
-public enum TutorialLevels
-{
-    Level1,
-    Level2,
-    Level3
-}
+
 public class TutorialManager : MonoBehaviour
 {
 
     [SerializeField] private TextMeshProUGUI tutorialText;
     [SerializeField] private MeshRenderer darkenStencil;
+    [SerializeField] private Image backgroundImage;
     private bool isDarkened = false;
     private int currentLevel;
+    private Color darkenColor = new Color(0.3f, 0.3f, 0.3f);
+    private Color normalColor = Color.white;
     void OnEnable()
     {
         GameEvents.OnGameStart += StartTutorial;
@@ -39,17 +38,16 @@ public class TutorialManager : MonoBehaviour
     {
         currentLevel = GameManager.Instance.CurrentLevel;
 
-        TutorialLevels tutorialLevel = (TutorialLevels)currentLevel;
-        switch (tutorialLevel)
+        switch (currentLevel)
         {
 
-            case TutorialLevels.Level1:
+            case 1:
                 ActivateTutorialCamera("Level1 Tutorial");
                 break;
-            case TutorialLevels.Level2:
+            case 2:
                 ActivateTutorialCamera("Level2 Tutorial");
                 break;
-            case TutorialLevels.Level3:
+            case 3:
                 ActivateTutorialCamera("Level3 Tutorial");
                 break;
             default:
@@ -63,6 +61,7 @@ public class TutorialManager : MonoBehaviour
         //  tutorialCamera.SetActive(true);
         tutorialText.text = message;
         tutorialText.gameObject.SetActive(true);
+        backgroundImage.color = darkenColor;
 
         // MeshRenderer'ı etkinleştir
         if (darkenStencil != null)
@@ -76,7 +75,7 @@ public class TutorialManager : MonoBehaviour
     {
         // tutorialCamera.SetActive(false);
         tutorialText.gameObject.SetActive(false);
-
+        backgroundImage.color = normalColor;
         // Volume weight'ini 0 yap
         if (darkenStencil != null)
         {
